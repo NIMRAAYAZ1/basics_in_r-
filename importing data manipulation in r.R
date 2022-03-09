b19a5cca3fa9d58e632ebdcd65f8b612d8e85ef3
Nimra <96565927+NIMRAAYAZ1@users.noreply.github.com>
2022-03-10 00:04:30 +0500

    DATA FRAMES IN R

---
+++ importing data manipulation in r.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+booking_df <- (GL0bk8O2Sja9G5PDtko2uQ_31e445d7ca64417eb45aeaa08ec90bf1_hotel_bookings_3_)
+ # Now again using the common function 
+head(booking_df)
+
+#now lets check the summary of the function 
+
+str(booking_df)
+
+# now lets check the degree name 
+colnames(bookings_df)
+
+#if we want to create a new average data frame then we used the focus on average daily frames 
+ booking_df <- 
